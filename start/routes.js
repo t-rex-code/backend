bd6989ba47f3ffc97a25a6b3b7ed95e6e4cca3c7
Douglas Martins Santos <dougprogrammer@gmail.com>
2019-10-09 12:36:31 -0300

    feature: CRUD de times

--- start/routes.js
+++ start/routes.js
@@ -16,4 +16,8 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.post('sessions', 'SessionController.store')
+Route.post('sessions', 'SessionController.store ')
+
+Route.group(() => {
+  Route.resource('teams', 'TeamController').apiOnly()
+}).middleware('auth')
